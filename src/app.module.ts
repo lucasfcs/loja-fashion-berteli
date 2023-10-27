85154d2457e7230de65772694e6b716a3e541f94
lucasfcs <lucas.fcs@outlook.com>
2023-10-26 23:29:15 -0300

    adding schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { ProductModule } from './modules/product/product.module';
+import { MovementsModule } from './modules/movements/movements.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ProductModule, MovementsModule],
   controllers: [AppController],
   providers: [AppService],
 })
